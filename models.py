1cb082243ecef6d7936c5557531d12cc21d567e9
Bursoap <andrei_kulikov@icloud.com>
2019-09-19 17:00:16 +0300

    created m2m

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, \
+    Table
 from sqlalchemy.orm import relationship
 
 engine = create_engine('mysql://kino:kino@172.17.0.2/movie_rental', echo=True)
@@ -26,6 +27,14 @@ class Location(Base):
     number = Column(String(100))
 
 
+office_department = Table(
+    'office_department',
+    Base.metadata,
+    Column('office_id', Integer, ForeignKey('office.id')),
+    Column('department_id', Integer, ForeignKey('department.id'))
+)
+
+
 class Office(Base):
     __tablename__ = 'office'
 
@@ -35,6 +44,11 @@ class Office(Base):
     office_head_id = Column(ForeignKey('employee.id'))
     office_head = relationship('Employee', backref='subordinate_offices')
     name = Column(String(50), nullable=False, unique=True)
+    departments = relationship(
+        'Department',
+        secondary=office_department,
+        back_populates='offices'
+    )
 
 
 class Department(Base):
@@ -42,10 +56,8 @@ class Department(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=True, unique=True)
-
-
-class OfficeDepartment(Base):
-    __tablename__ = 'office_department'
-
-    office_id = Column(ForeignKey('office.id'), primary_key=True)
-    office = relationship('Office', backref=)
+    offices = relationship(
+        'Office',
+        secondary=office_department,
+        back_populates='departments'
+    )

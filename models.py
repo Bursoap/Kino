e2f12bd9cef33a78211b34521a947d58873f4d63
Bursoap <andrei_kulikov@icloud.com>
2019-10-02 16:44:15 +0300

    added film model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
+from sqlalchemy.dialects.mysql import DOUBLE
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, \
-    Table, Date, TIMESTAMP, func, Boolean
+    Table, Date, TIMESTAMP, func, Boolean, Time
 from sqlalchemy.orm import relationship, backref
 
 engine = create_engine('mysql://kino:kino@172.17.0.2/movie_rental', echo=True)
@@ -21,6 +22,7 @@ class Location(Base):
     id = Column(Integer, primary_key=True)
     country_id = Column(Integer, ForeignKey('country.id'), nullable=False)
     country = relationship('Country', backref='locations')
+
     state = Column(String(100))
     city = Column(String(100))
     street = Column(String(100))
@@ -41,8 +43,10 @@ class Office(Base):
     id = Column(Integer, primary_key=True)
     location_id = Column(Integer, ForeignKey('location.id'), nullable=False)
     location = relationship('Location', backref='offices')
-    office_head_id = Column(Integer, ForeignKey('employee.id'))
+
+    office_head_id = Column(Integer, ForeignKey('employee.id', use_alter=True))
     office_head = relationship('Employee', backref='subordinate_offices')
+
     name = Column(String(50), nullable=False, unique=True)
     departments = relationship(
         'Department',
@@ -69,6 +73,7 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     location_id = Column(Integer, ForeignKey('location.id'))
     location = relationship('Location', backref='persons')
+
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     birth_date = Column(Date, nullable=False)
@@ -79,8 +84,86 @@ class Client(Base):
     __tablename__ = 'client'
 
     id = Column(Integer, primary_key=True)
-    person_id = Column(Integer, ForeignKey('person.id'))
+    person_id = Column(Integer, ForeignKey('person.id'),
+                       nullable=False, unique=True)
     person = relationship('Person', backref=backref('client', uselist=False))
+
     created = Column(TIMESTAMP, nullable=False, server_default=func.now())
     email = Column(String(100))
     active = Column(Boolean, nullable=False, default=True)
+
+
+class Actor(Base):
+    __tablename__ = 'actor'
+
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'),
+                       nullable=False, unique=True)
+    person = relationship('Person', backref=backref('actor', uselist=False))
+
+    biography = Column(String(10000))
+    oscar = Column(Boolean, nullable=False, default=True)
+
+
+class Employee(Base):
+    __tablename__ = 'employee'
+
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'),
+                       nullable=False, unique=True)
+    person = relationship('Person', backref=backref('employee', uselist=False))
+
+    office_id = Column(Integer, ForeignKey('office.id'), nullable=False)
+    office = relationship('Office', backref='employees')
+
+    dept_id = Column(Integer, ForeignKey('department.id'), nullable=False)
+    department = relationship('Department', backref='employees')
+
+    title = Column(String(100), nullable=False)
+    salary = Column(DOUBLE(precision=8, scale=2), nullable=False)
+    active = Column(Boolean, nullable=False, default=True)
+    employment_date = Column(Date, nullable=False)
+    dismiss_date = Column(Date)
+
+
+film_genre = Table(
+    'film_genre',
+    Base.metadata,
+    Column('film_id', Integer, ForeignKey('film.id')),
+    Column('genre_id', Integer, ForeignKey('genre.id'))
+)
+
+
+class Genre(Base):
+    __tablename__ = 'genre'
+
+    id = Column(Integer, primary_key=True)
+    films = relationship(
+        'Film',
+        secondary=film_genre,
+        back_populates='genres'
+    )
+
+    name = Column(String(50), nullable=False, unique=True)
+
+
+class Film(Base):
+    __tablename__ = 'film'
+
+    id = Column(Integer, primary_key=True)
+    producer_id = Column(Integer, ForeignKey('person.id'), nullable=False)
+    producer = relationship('Person', backref='produced_films')
+
+    country_id = Column(Integer, ForeignKey('country.id'), nullable=False)
+    country = relationship('Country', backref='films')
+
+    genres = relationship(
+        'Genre',
+        secondary=film_genre,
+        back_populates='films'
+    )
+
+    name = Column(String(50), nullable=False)
+    description = Column(String(10000))
+    duration = Column(Time, nullable=False)
+    release_date = Column(Date, nullable=False)
